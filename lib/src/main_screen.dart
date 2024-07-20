2f2d048b32ccf9509e5b44b882f215548e4776ea
nexdev <anwarnasir0970@gmail.com>
2024-07-20 21:55:58 +0300

    initial commit

---
+++ lib/src/main_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:greenhouse/src/view/home/home_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // List of widgets to display in the body of Scaffold, based on the selected index
+  final List<Widget> _widgetOptions = [
+     HomePage(), // Assuming HomePage is one of your pages
+    // Add other pages here, e.g., SearchPage(), ProfilePage(), etc.
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_sharp),
+            label: 'Home',
+          ),
+
+          // Add other items for your pages here
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
